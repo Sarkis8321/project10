35003f9353931088865d3ef89612f237ac6babe2
Sarkis8321 <danielyan.sar@yandex.ru>
2020-12-20 12:29:40 +0400

    Табы для разработчиков

--- Unit1.pas
+++ Unit1.pas
@@ -8,11 +8,26 @@ type
   Form1 = class(Form)
     procedure button1_Click(sender: Object; e: EventArgs);
     procedure button2_Click(sender: Object; e: EventArgs);
+    procedure toolStripMenuItem5_Click(sender: Object; e: EventArgs);
+    procedure toolStripMenuItem6_Click(sender: Object; e: EventArgs);
   {$region FormDesigner}
   internal
     {$resource Unit1.Form1.resources}
     button1: Button;
     button2: Button;
+    tabControl1: TabControl;
+    tabPage1: TabPage;
+    tabPage2: TabPage;
+    menuStrip1: MenuStrip;
+    toolStripMenuItem1: ToolStripMenuItem;
+    toolStripMenuItem3: ToolStripMenuItem;
+    toolStripMenuItem2: ToolStripMenuItem;
+    toolStripMenuItem5: ToolStripMenuItem;
+    toolStripMenuItem6: ToolStripMenuItem;
+    textBox3: TextBox;
+    textBox2: TextBox;
+    comboBox1: ComboBox;
+    comboBox2: ComboBox;
     textBox1: TextBox;
     {$include Unit1.Form1.inc}
   {$endregion FormDesigner}
@@ -29,14 +44,39 @@ implementation
 
 procedure Form1.button1_Click(sender: Object; e: EventArgs);
 begin
-  operation := 1;
-  prev := StrToFloat(textbox1.Text);
-  textbox1.Text := '';
+  
+  try
+    operation := 1;
+    prev := StrToFloat(textbox1.Text);
+    textbox1.Text := '';
+  except
+    on System.FormatException do
+    textbox1.Text := 'Неверный формат ввода';
+  else textbox1.Text := 'Ошибка';
+  end;
+  textbox1.Focus;
+  
 end;
 
 procedure Form1.button2_Click(sender: Object; e: EventArgs);
 begin
+  var res := 0.0;
+  if operation = 1 then 
+    res := prev + StrToFloat(textbox1.Text);
   
+  textbox1.Focus;
+  textbox1.Text := res.ToString;
+  
+end;
+
+procedure Form1.toolStripMenuItem5_Click(sender: Object; e: EventArgs);
+begin
+  tabControl1.SelectedTab := tabPage1;
+end;
+
+procedure Form1.toolStripMenuItem6_Click(sender: Object; e: EventArgs);
+begin
+  tabControl1.SelectedTab := tabPage2;
 end;
 
 end.
